1262ec5bff2f387a7ec7454345f6196eb7dfe1d6
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-10-05 20:18:17 +0300

    Updated FlashMessages.

--- src/components/FlashMessage.js
+++ src/components/FlashMessage.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Button from './Button';
 import Icon from './Icon';
 
 const FlashMessage = ({ message, onDelete }) => {
 	return (
 		<div className={"flash-message " + message.type}>
 			<p>{message.text}</p>
-			<Button onClick={() => onDelete(message.id)}><Icon name="times" /></Button>
+			<button onClick={() => onDelete(message.id)}><Icon name="times" /></button>
 		</div>
 	);
 };
--- src/components/FlashMessagesList.js
+++ src/components/FlashMessagesList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { deleteFlashMessage } from '../actions/flashMessagesActions';
 
@@ -6,20 +6,18 @@ import FlashMessage from './FlashMessage';
 
 import '../styles/FlashMessages.css';
 
-class FlashMessagesList extends Component {
-	render() {
-		return (
-			<div className={this.props.messages.length === 0
-				? "flash-messages mt-0"
-				: "flash-messages"}
-			>
-				{this.props.messages.length > 0 && this.props.messages.map(message => {
-					return <FlashMessage key={message.id} message={message} onDelete={this.props.deleteFlashMessage} />
-				})}
-			</div>
-		);
-	}
-}
+const FlashMessagesList = ({ messages, deleteFlashMessage }) => {
+	return (
+		<div className={messages.length === 0
+			? "flash-messages mt-0"
+			: "flash-messages"}
+		>
+			{messages.length > 0 && messages.map(message => {
+				return <FlashMessage key={message.id} message={message} onDelete={deleteFlashMessage} />
+			})}
+		</div>
+	);
+};
 
 const mapStateToProps = state => {
 	return {
