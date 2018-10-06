70474b2b649e5e6bb16baab733d8208069a135df
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-10-06 23:26:08 +0300

    Created AddProduct component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import PrivateRoute from './components/PrivateRoute';
 import Dashboard from './components/Dashboard';
+import AddProduct from './components/AddProduct';
 import ViewProduct from './components/ViewProduct';
 import Customers from './components/Customers';
 import Stats from './components/Stats';
@@ -14,6 +15,7 @@ const App = () => {
 			<Switch>
 				<Route path="/login" component={Auth} />
 				<PrivateRoute exact path="/" component={Dashboard} />
+				<PrivateRoute exact path="/add-product" component={AddProduct} />
 				<PrivateRoute exact path="/view-product/:id" component={ViewProduct} />
 				<PrivateRoute path="/customers" component={Customers} />
 				<PrivateRoute path="/stats" component={Stats} />
---
+++ src/components/AddProduct.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Sidebar from './Sidebar';
+import Content from './Content';
+import Icon from './Icon';
+
+import '../styles/AddProduct.css';
+
+class AddProduct extends Component {
+	state = {
+		productName: '',
+		category: '',
+		productPrice: '',
+		productDescription: '',
+		amountLeft: '',
+		imgUrl: ''
+	}
+
+	handleChange = e => {
+		this.setState({ [e.target.name]: e.target.value });
+	}
+
+	handleSubmit = e => {
+		e.preventDefault();
+		const {
+			productName,
+			category,
+			productPrice,
+			productDescription,
+			amountLeft,
+			imgUrl
+		} = this.state;
+		
+		alert(productName + '\n' + category + '\n' + productPrice + '\n' + productDescription + '\n' + amountLeft + '\n' + imgUrl);
+	}
+
+	render() {
+		return (
+			<div className="wrapper">
+				<Sidebar />
+				<Content>
+					<div className="page-header block">
+						<h1>Products</h1>
+					</div>
+					<form className="block form" onSubmit={this.handleSubmit}>
+						<input
+							type="text"
+							name="productName"
+							placeholder="Product name"
+							onChange={this.handleChange}
+							required />
+						<input
+							type="text"
+							name="category"
+							placeholder="Category"
+							onChange={this.handleChange}
+							required />
+						<input
+							type="number"
+							name="productPrice"
+							placeholder="Price"
+							onChange={this.handleChange}
+							required />
+						<input
+							type="text"
+							name="productDescription"
+							placeholder="Description"
+							onChange={this.handleChange}
+							required />
+						<input
+							type="number"
+							name="amountLeft"
+							placeholder="Amount at warehouse"
+							onChange={this.handleChange}
+							required />
+						<input
+							type="text"
+							name="imgUrl"
+							placeholder="Url to product photo"
+							onChange={this.handleChange}
+							required />
+						<button type="submit"><Icon name="plus" />Add product</button>
+					</form>
+				</Content>
+			</div>
+		);
+	}
+}
+
+export default connect(null)(AddProduct);
