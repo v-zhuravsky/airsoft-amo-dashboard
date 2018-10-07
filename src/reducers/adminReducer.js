61890c9492ca946454bf428c583aa46bb510dff6
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-10-08 01:18:01 +0300

    Added some initial state to adminReducer (just for development).

--- src/reducers/adminReducer.js
+++ src/reducers/adminReducer.js
@@ -1,4 +1,8 @@
 const initialState = {
+	firstName: 'Alex',
+	lastName: 'Willson',
+	avatar: 'https://assets.gitlab-static.net/uploads/-/system/user/avatar/872331/avatar.png?width=90',
+	type: 'Administrator',
 	accessToken: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
 };
 
