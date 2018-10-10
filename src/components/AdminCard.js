38e28ac26d5467192cadcd716b614e08af474f2e
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-10-10 16:17:57 +0300

    Finished working on Admins and AdminCard components.

--- src/components/AdminCard.js
+++ src/components/AdminCard.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Img from './Img';
 import Icon from './Icon';
 
 import '../styles/AdminCard.css';
 
-const AdminCard = ({ admin }) => {
+const AdminCard = ({ admin, currentAdminId }) => {
 	return (
 		<div className="col-md-3">
 			<div className="admin-card">
 				<Img src={admin.avatar} alt={`${admin.firstName} ${admin.lastName}`} className="img-responsive img-circle" />
-				<h2><Icon name="user-secret" />{admin.firstName} {admin.lastName}</h2>
-				<p>{admin.type}</p>
+				<h2>
+					<Link to={"/admin/" + admin.id}>
+						<Icon name="user-secret" />{admin.firstName} {admin.lastName}
+					</Link>
+				</h2>
+				{
+					currentAdminId === admin.id
+						? <p className="you">You</p>
+						: <p className="admin">{admin.type}</p>
+				}
 			</div>
 		</div>
 	);
