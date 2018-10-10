5b5f6410b243015348c4177e59ad16b19b3a5b4e
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-10-10 23:38:15 +0300

    Added some initial data for orders

--- src/reducers/ordersReducer.js
+++ src/reducers/ordersReducer.js
@@ -8,7 +8,52 @@ const initialState = {
 		{name: 'Tactical gear', productsSold: 165},
 		{name: 'Lanterns and light devices', productsSold: 78}
 	],
-	orders: []
+	orders: [
+		{
+			id: 1,
+			name: 'Alex Green',
+			phoneNumber: '+380662104314',
+			deliveryLocation: 'location 1',
+			products: [
+				{
+					productId: 1,
+					amount: 1,
+					productName: "M16A3 Katana System [WE]",
+					category: "rifles",
+					productPrice: "606"
+				},
+				{
+					productId: 2,
+					amount: 10,
+					productName: "AKC-74M E&L Gen2",
+					category: "rifles",
+					productPrice: "407"
+				}
+			]
+		},
+		{
+			id: 2,
+			name: 'Brad Traversy',
+			phoneNumber: '+380662105634',
+			deliveryLocation: 'location 2',
+			products: [
+				{
+					productId: 1,
+					amount: 4,
+					productName: "M16A3 Katana System [WE]",
+					category: "rifles",
+					productPrice: "606"
+				},
+				{
+					productId: 2,
+					amount: 23,
+					productName: "AKC-74M E&L Gen2",
+					category: "rifles",
+					productPrice: "407"
+				}
+			]
+		}
+	]
 };
 
 const ordersReducer = (state = initialState, action) => {
