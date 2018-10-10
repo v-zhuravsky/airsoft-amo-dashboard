078a4ea53a74f891a7ccededbee9c7e56ee3f7ed
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-10-10 17:21:01 +0300

    Created route to Admin component.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import Dashboard from './components/Dashboard';
 import Admins from './components/Admins';
+import Admin from './components/Admin';
 import AddProduct from './components/AddProduct';
 import ViewProduct from './components/ViewProduct';
 import Customers from './components/Customers';
@@ -18,6 +19,7 @@ const App = () => {
 				<Route path="/login" component={Auth} />
 				<PrivateRoute exact path="/" component={Dashboard} />
 				<PrivateRoute path="/admins" component={Admins} />
+				<PrivateRoute path="/admin/:id" component={Admin} />
 				<PrivateRoute path="/add-product" component={AddProduct} />
 				<PrivateRoute path="/view-product/:id" component={ViewProduct} />
 				<PrivateRoute path="/customers" component={Customers} />
