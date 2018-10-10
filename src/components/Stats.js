6c99ae1f30b4ca76417c798f934df55103e9a8f0
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-10-10 22:43:10 +0300

    Put chart on Stats page

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Sidebar from './Sidebar';
 import Content from './Content.js';
 import ToggleSidebar from './ToggleSidebar';
 import FlashMessagesList from './FlashMessagesList';
+import OrdersStats from './OrdersStats';
 
-const Stats = () => {
+import '../styles/Stats.css';
+
+const Stats = ({ stats }) => {
 	return (
 		<div className="wrapper">
 			<Sidebar />
@@ -15,9 +19,19 @@ const Stats = () => {
 					<h2>Stats</h2>
 				</div>
 				<FlashMessagesList />
+				<div className="block orders-stats">
+					<h2>Sales by categories stats</h2>
+					<OrdersStats width={950} height={350} stats={stats} />
+				</div>
 			</Content>
 		</div>
 	);
 };
 
-export default Stats;
+const mapStateToProps = state => {
+	return {
+		stats: state.orders.stats
+	};
+};
+
+export default connect(mapStateToProps)(Stats);
