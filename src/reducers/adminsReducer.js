a388368721f8e0c007e5c992583846db07286fbd
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-10-10 17:28:30 +0300

    Updated some initialState data

--- src/reducers/adminsReducer.js
+++ src/reducers/adminsReducer.js
@@ -3,7 +3,7 @@ const initialState = [
 		id: 1,
 		firstName: 'Alex',
 		lastName: 'Willson',
-		avatar: 'https://assets.gitlab-static.net/uploads/-/system/user/avatar/872331/avatar.png?width=90',
+		avatar: 'https://avatars.mds.yandex.net/get-pdb/1004346/071d2113-23f4-4b37-b7ce-342ab01c0d6b/orig',
 		type: 'Administrator',
 		adminMonthsActivity: [
 			{name: 'December', completed: 632, canceled: 243},
