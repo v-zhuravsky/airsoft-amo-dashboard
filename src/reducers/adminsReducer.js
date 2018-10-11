881ddc2bbc45b00edf3ae418cfcaeef1059befdb
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-10-11 19:07:19 +0300

    Updated avatar of first admin.

--- src/reducers/adminsReducer.js
+++ src/reducers/adminsReducer.js
@@ -5,7 +5,7 @@ const initialState = [
 		id: 1,
 		firstName: 'Alex',
 		lastName: 'Willson',
-		avatar: 'https://avatars.mds.yandex.net/get-pdb/1004346/071d2113-23f4-4b37-b7ce-342ab01c0d6b/orig',
+		avatar: 'https://cdn4.telesco.pe/file/PpTaXns0T7b5xv0nWfMJ_w5f13nwm-F_9td1-Qn3p3toHUqE6nz5Qt1LNb5CyBNSBhJjUJm77cbKWXeHIFDT7QsJnXSBC7UDhNCqaBG3lGoJ0JWGvMV9gekLAqVA2xUutIH_xqtZP0pbz4l69Kv38BS-ZVTQuzQybY1JFefXbzeX08BuIrcVA3z9yraURFmveW3G-yunRSIHm-3tpfy05BUFDB7fcLPfeKwxgowNi1ZGwEwpwPo7QW5yOzGsud6HRsi_bzS-UBhxv-O-uc_G6_r8JrpLufqv6H5pXth73CcBTACwJaxmmY-hcEQyWAAEpRenI5VsYeEeLm8TkwLNLQ.jpg',
 		type: 'Administrator',
 		adminMonthsActivity: [
 			{name: 'December', completed: 632, canceled: 243},
