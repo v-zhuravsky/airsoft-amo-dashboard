10d1c8923dd3fe26a10dcf364eab38616d48a10d
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-10-16 09:04:03 +0300

    Added link to create admin page

--- src/components/Admins.js
+++ src/components/Admins.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import Sidebar from './Sidebar';
 import Content from './Content.js';
 import ToggleSidebar from './ToggleSidebar';
 import AdminCard from './AdminCard';
 import FlashMessagesList from './FlashMessagesList';
+import Icon from './Icon';
 
 const Admins = ({ admins, currentAdminId }) => {
 	return (
@@ -15,6 +17,7 @@ const Admins = ({ admins, currentAdminId }) => {
 				<div className="page-header block">
 					<ToggleSidebar />
 					<h2>Admins</h2>
+					<Link to="/add-admin"><Icon name="plus" />Add new admin</Link>
 				</div>
 				<FlashMessagesList />
 				{
