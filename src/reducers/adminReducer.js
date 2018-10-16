c24bb7e50bcd85f5552c56260f8472d31cd36d60
Viacheslav Zhuravskiy <zhuravsky_v@a.krok.edu.ua>
2018-10-16 08:42:52 +0300

    Updated some initial state

--- src/reducers/adminReducer.js
+++ src/reducers/adminReducer.js
@@ -4,7 +4,7 @@ const initialState = {
 	id: 1,
 	firstName: 'Alex',
 	lastName: 'Willson',
-	avatar: 'https://cdn4.telesco.pe/file/PpTaXns0T7b5xv0nWfMJ_w5f13nwm-F_9td1-Qn3p3toHUqE6nz5Qt1LNb5CyBNSBhJjUJm77cbKWXeHIFDT7QsJnXSBC7UDhNCqaBG3lGoJ0JWGvMV9gekLAqVA2xUutIH_xqtZP0pbz4l69Kv38BS-ZVTQuzQybY1JFefXbzeX08BuIrcVA3z9yraURFmveW3G-yunRSIHm-3tpfy05BUFDB7fcLPfeKwxgowNi1ZGwEwpwPo7QW5yOzGsud6HRsi_bzS-UBhxv-O-uc_G6_r8JrpLufqv6H5pXth73CcBTACwJaxmmY-hcEQyWAAEpRenI5VsYeEeLm8TkwLNLQ.jpg',
+	avatar: 'https://assets.gitlab-static.net/uploads/-/system/user/avatar/872331/avatar.png?width=90',
 	type: 'Administrator',
 	accessToken: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
 };
